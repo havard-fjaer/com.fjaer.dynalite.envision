c6a4f33596755930d5cc4d033e652be8d069cde6
Håvard Fjær <github@fjaer.com>
2025-02-09 20:49:25 +0100

    DynaliteSocket

--- drivers/socket/device.ts
+++ drivers/socket/device.ts
@@ -1,55 +1,6 @@
-import Homey from 'homey';
+import DynaliteDevice from '../DynaliteDevice';
 
-module.exports = class SocketDevice extends Homey.Device {
+export class DynaliteSocket extends DynaliteDevice {
 
-  /**
-   * onInit is called when the device is initialized.
-   */
-  async onInit() {
-    this.log('Socket Device has been initialized');
-  }
-
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
-  async onAdded() {
-    this.log('Socket Device has been added');
-  }
-
-  /**
-   * onSettings is called when the user updates the device's settings.
-   * @param {object} event the onSettings event data
-   * @param {object} event.oldSettings The old settings object
-   * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
-   * @returns {Promise<string|void>} return a custom message that will be displayed
-   */
-  async onSettings({
-    oldSettings,
-    newSettings,
-    changedKeys,
-  }: {
-    oldSettings: { [key: string]: boolean | string | number | undefined | null };
-    newSettings: { [key: string]: boolean | string | number | undefined | null };
-    changedKeys: string[];
-  }): Promise<string | void> {
-    this.log("Socket Device settings where changed");
-  }
-
-  /**
-   * onRenamed is called when the user updates the device's name.
-   * This method can be used this to synchronise the name to the device.
-   * @param {string} name The new name
-   */
-  async onRenamed(name: string) {
-    this.log('Socket Device was renamed');
-  }
-
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
-  async onDeleted() {
-    this.log('Socket Device has been deleted');
-  }
-
-};
+}
+module.exports = DynaliteSocket;
